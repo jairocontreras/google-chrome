885c4af03aac76d0afe68b981b1a25cdf33e904e
jairocontreras <43326330+jairocontreras@users.noreply.github.com>
2023-01-16 21:39:43 -0800

    update

--- set as desktop background/extension/download.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-  document.getElementById("script").addEventListener("click", function() {
-    chrome.downloads.download({url: "chrome-extension://" + chrome.runtime.id + "/Script Installer.exe"});
-  });
-});
