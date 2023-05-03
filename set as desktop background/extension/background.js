6d218e2af6b3fe543af604c1b25779c1c45cd3b2
jairocontreras <43326330+jairocontreras@users.noreply.github.com>
2023-05-02 20:48:42 -0700

    update

--- set as desktop background/extension/background.js
+++ set as desktop background/extension/background.js
@@ -1,8 +1,15 @@
 chrome.runtime.onInstalled.addListener(details => {
   if (details.reason == "install")
-    chrome.tabs.create({url:"main.html"});
-  else if (details.reason == "update")
-    chrome.tabs.create({url:"update.html"});
+    chrome.tabs.create({url: "main.html"});
+  else if (details.reason == "update") {
+    chrome.notifications.create({
+      buttons: [{title: "Download"}],
+      iconUrl: "60.png",
+      message: "Update available for the script",
+      title: "Set as desktop background",
+      type: "basic"
+    });
+  }
   chrome.contextMenus.create({
     "id": "id",
     "title": "Set as desktop background",
@@ -10,6 +17,19 @@ chrome.runtime.onInstalled.addListener(details => {
   });
 });
 
+chrome.notifications.onButtonClicked.addListener(() => {
+  chrome.tabs.create({url: "https://bit.ly/3AM8By8"});
+});
+
 chrome.contextMenus.onClicked.addListener(info => {
-  chrome.runtime.sendNativeMessage("set_as_desktop_background", {url: info.srcUrl});
+  chrome.runtime.sendNativeMessage("set_as_desktop_background", {url: info.srcUrl}, response => {
+    if (response) {
+      chrome.notifications.create({
+        iconUrl: "error.png",
+        message: response,
+        title: "Failed to download file",
+        type: "basic"
+      });
+    }
+  });
 });
--- set as desktop background/script/host.py
+++ set as desktop background/script/host.py
@@ -1,4 +1,4 @@
-import ctypes, json, os, shutil, struct, sys, tempfile, urllib.error, urllib.request
+import ctypes, json, os, shutil, struct, sys, tempfile, urllib.request
 
 text_length_bytes = sys.stdin.buffer.read(4)
 text_length = struct.unpack("i", text_length_bytes)[0]
@@ -8,20 +8,23 @@ url = message["url"]
 
 for i in range(2):
   try:
-    with urllib.request.urlopen(url) as httpresponse, tempfile.NamedTemporaryFile(delete=False) as temp:
-      shutil.copyfileobj(httpresponse, temp)
+    with urllib.request.urlopen(url) as response, tempfile.NamedTemporaryFile(delete=False) as temp:
+      shutil.copyfileobj(response, temp)
   except urllib.error.HTTPError as e:
-    # python user agent blocked by server
-    if "HTTP Error 403: Forbidden" in str(e):
-      url = urllib.request.Request(url, headers={"User-Agent": "Mozilla/5.0"})
+    error = str(e)
+    if error == "HTTP Error 403: Forbidden":
+      if i == 0:
+        url = urllib.request.Request(url, headers={"user-agent": "mozilla/5.0"})
+      else:
+        response = error
   else:
     # SPI_SETDESKWALLPAPER, SPIF_UPDATEINIFILE, SPIF_SENDCHANGE
     ctypes.windll.user32.SystemParametersInfoW(0x0014, 0, temp.name, 0x01 | 0x02)
     os.remove(temp.name)
+    response = ""
     break
 
-# prevent error: native host has exited
-response = "{}" # must be json format to prevent error: STATUS_ACCESS_VIOLATION
-sys.stdout.buffer.write(struct.pack("i", len(response)))
-sys.stdout.write(response)
+text = json.dumps(response)
+sys.stdout.buffer.write(struct.pack("i", len(text)))
+sys.stdout.write(text)
 sys.stdout.flush()
