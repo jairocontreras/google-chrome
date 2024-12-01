b6f05ae0c02c066ee61b423ad053a249cdcf7438
jairocontreras <43326330+jairocontreras@users.noreply.github.com>
2024-12-01 10:08:20 -0800

    update

--- set as desktop background/extension/script/main.js
+++ set as desktop background/extension/script/main.js
@@ -1,13 +1,20 @@
 chrome.runtime.sendNativeMessage("set_as_desktop_background", {url: ""}, response => {
   const lasterror = chrome.runtime.lastError,
-        hidden = document.querySelector(".hidden"),
+        status = document.getElementById("status"),
         heading = document.getElementById("heading"),
         install = document.getElementById("install");
   if (!lasterror) {
-    hidden.classList.add("animate");
+    status.classList.add("animate");
     heading.textContent = "Congrats!";
     install.textContent = "The following components are already installed:";
     document.querySelectorAll(".button").forEach(elem => {elem.classList.add("installed")});
   }
-  hidden.classList.remove("hidden");
+  status.classList.remove("hidden");
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("clear").addEventListener("click", () => {
+    chrome.storage.local.clear();
+    document.getElementById("done").classList.remove("hidden");
+  });
 });
